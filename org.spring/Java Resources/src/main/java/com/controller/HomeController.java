763beae6a85430a535d2922a0e6f3a19a807f4d7
Pratik Mahadev Dimble <31777260+pratikdimble@users.noreply.github.com>
2018-06-08 16:51:12 +0530

    Create HomeController.java

---
+++ org.spring/Java Resources/src/main/java/com/controller/HomeController.java
@@ -0,0 +1,64 @@
+package com.controller;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.delegate.Employee_Delegate;
+
+
+
+@Controller
+public class HomeController {
+
+@RequestMapping(value="/")
+public String landing(HttpServletRequest req,HttpServletResponse res)
+{
+	
+	return "/views/login_form";
+
+}
+
+
+   @RequestMapping(value="showLogin")
+	public String showLogin(HttpServletRequest req,HttpServletResponse res)
+	{
+		
+		return "/login_form";
+	
+}
+   
+   @RequestMapping(value="login")
+   public ModelAndView showData(HttpServletRequest req,HttpServletResponse res)
+	{
+	   String name=req.getParameter("name");
+		double salary=Double.parseDouble(req.getParameter("salary"));
+		
+		 		//use Delegagate
+		Employee_Delegate delegate=null;
+		String 	insertResult=null;
+		RequestDispatcher rd=null;
+		delegate=new Employee_Delegate ();
+			ModelAndView mv=new ModelAndView();
+ 	  try{
+ 	  insertResult=delegate.addEmployee(name,salary);
+
+ 	  }//try close
+
+ 	  catch(Exception e){
+ 		  req.setAttribute("errMsg",e.getMessage());
+
+ 	  }//catch
+ 	mv.setViewName("/views/display");
+	mv.addObject("insertResult",insertResult);
+	return mv;
+	
+}
+}
